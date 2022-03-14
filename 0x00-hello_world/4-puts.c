d70b64d60f9d1928b573931d52673fd6f8f24aae
Lionel Tangang <tanganglionel@gmail.com>
2022-03-14 15:58:01 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-
 
 /**
 
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Description: main - prints out a string to stdout.
 
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if success.
 
  */
 
