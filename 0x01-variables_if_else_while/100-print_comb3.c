89de9467da5ffbcab2502b5ad7e5bcdaa26a959c
Lionel Tangang <tanganglionel@gmail.com>
2022-03-14 19:09:39 -0700

    print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,38 +10,44 @@ int main(void)
 
 {
 
-    
+	int digit1, digit2;
 
-	for (int i = 0; i <= 9; i++)
+
+
+	for (digit1 = 0; digit1 < 9; digit1++)
 
 	{
 
-	    for (int j = i + 1; j <= 9; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 
-	    {
+		{
 
-	        putchar(i + '0');
+			putchar((digit1 % 10) + '0');
 
-	        putchar(j + '0');
-
-	        if (((i * 10) + j) < 89)
-
-	        {
-
-	            putchar(',');
-
-                putchar(' ');
+			putchar((digit2 % 10) + '0');
 
 
 
-	        }
+			if (digit1 == 8 && digit2 == 9)
 
-	    }
+				continue;
+
+
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
 
 	}
 
+
+
 	putchar('\n');
 
-    return 0;
+
+
+	return (0);
 
 }
