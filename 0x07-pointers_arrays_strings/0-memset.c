e24ad23004922b8f9458eaa3e4190ae9577e72d1
Lionel Tangang <tanganglionel@gmail.com>
2022-05-02 07:53:25 -0700

    Corrected betty on 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * *_memset - fills memory with a constant byte
  * @s: memory area
  * @b: constant byte filled in the memory
- * @n: first bytes of the memory area pointed 
+ * @n: first bytes of the memory area pointed
  * by s to be filled with constant byte b
  *
  * Return: a pointer to memory area s
