aa4f11c1cb43d9559049a4292e42174d5108340c
Lionel Tangang <tanganglionel@gmail.com>
2022-05-02 08:31:48 -0700

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @c: character to be located
+ * @s: string to search in
+ *
+ * Return: first occurence of the character c in string s
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (s - i);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
