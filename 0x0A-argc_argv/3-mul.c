617ac9b3f99b8421bf5321feafb541fca03d2d7c
Lionel Tangang <tanganglionel@gmail.com>
2022-06-10 10:59:07 -0700

    Updated betty coding style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2;
