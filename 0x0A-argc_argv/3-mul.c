40cba09937f78d0919e0ed36c4ac769f9870914d
Lionel Tangang <tanganglionel@gmail.com>
2022-06-10 10:56:04 -0700

    Included the stdlib header file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,42 +1,11 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * _atoi - converts a string to an integer
- * @s: String to convert
- *
- * Return: The int converted from the string
- */
-int _atoi(char *s)
-{
-	int sign = 1, result = 0, i = 0;
-
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i] != '\0'; ++i)
-	{
-		if (s[i] <= '0' || s[i] >= '9')
-			return (0);
-		result = result * 10 + s[i] - '0';
-	}
-	return (sign * result);
-}
-
-/**
- * main - multiplies two numbers
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Return:  0 (Success), 1 (Error)
- */
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
 	int num1, num2;
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	if (argc != 3)
 	{
 		printf("Error\n");
