0d88cfe62c88e6538388e272d848588b65ca4a5b
Lionel Tangang <tanganglionel@gmail.com>
2022-06-15 08:24:44 -0700

    Added README.md, main.h and 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array
+ * @c: character to initialize the array with
+ *
+ * Return: NULL if size = 0 or if it fails
+ * a pointer to the array (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		*(s + i) = c;
+		i++;
+	}
+	*(s + i) = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# Alx School project on Dynamic memory allocation in C
+## Using malloc and free functions
