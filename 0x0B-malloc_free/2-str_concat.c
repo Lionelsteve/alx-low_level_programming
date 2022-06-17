e244fbb0d4d4cb1200d8e9c0c65a3dab1a1e8073
Lionel Tangang <tanganglionel@gmail.com>
2022-06-17 04:47:05 -0700

    Modified 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,7 +29,7 @@ char *str_concat(char *s1, char *s2)
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
 	}
